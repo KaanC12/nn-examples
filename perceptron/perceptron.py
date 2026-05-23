3b67879ad7770f9211021f09b467d322fdf157bf
Kaan <kaan@Kaan-MacBook-Pro.local>
2026-05-23 20:10:45 +0100

    feat: plot added but pereptron is not working properly.

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -8,11 +8,11 @@ class Perceptron:
 
     def forward(self, inputs):
         self.inputs = inputs
-        self.output = self.inputs * self.weights + self.biases
+        self.outputs = self.inputs * self.weights + self.biases
 
     def backward(self, dvalues):
-        self.dweights = dvalues * self.weights
-        self.dbiasses = dvalues
+        self.dweights = np.sum(dvalues * self.inputs)
+        self.dbiasses = np.sum(dvalues)
 
 class MSE:
     def forward(self, y_input, y_prediction):
@@ -29,25 +29,51 @@ class MSE:
     (y - y')^2 : d/d'y (y - y')^2 = -2y + 2y'  
     """
     def backward(self):
-        self.dloss = -2 * self.y_input + 2 * self.y_prediction
+        self.dloss = -2 * self.y_input + 2 * self.y_prediction / len(self.y_input)
 
 
 
-
+# ================ DATA ==========
 x = np.array([x for x in range(1, 100)])
 y = 2 * x
 
+fig, ax = plt.subplots(2, 3, figsize=(12, 8))
+
+prediction = []
 
 # ============= MODEL =============
 perceptron = Perceptron()
 mse = MSE()
 
 epochs = 100 # Sicne the non-compleity of this problem, 100 will be enough.
-learning_rate = 0.01
+learning_rate = 0.001
+ax_x = 0
+ax_y = 0
 
+# ================= LEARNING PHASE ===============
 for epoch in range(epochs):
+    if epoch % 20 == 0:
+        perceptron.forward(x)
+
+        ax[ax_y, ax_x] = plt.plot(
+            x, y,
+            color="red",
+            label="Train Data"
+        )
+
+        ax[ax_y, ax_x] = plt.plot(
+            x, perceptron.outputs,
+            color="blue",
+            label="Prediction"
+        )
+        plt.legend()
+        ax_x += 1
+        if ax_x == 3:
+            ax_x = 0
+            ax_y += 1
+    
     perceptron.forward(x)
-    loss = mse.forward(perceptron.output, y)
+    loss = mse.forward(perceptron.outputs, y)
 
     mse.backward()
     perceptron.backward(mse.dloss)
@@ -55,5 +81,7 @@ for epoch in range(epochs):
     perceptron.weights -= learning_rate * perceptron.dweights
     perceptron.biases -= learning_rate * perceptron.dbiasses
 
+
+plt.show()
 
 
